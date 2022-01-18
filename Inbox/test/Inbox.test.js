8fa29d122fbbd59ef44660122321e64dc69c13e7
Andy Chen <andycjx8@gmail.com>
2022-01-18 11:39:31 +1100

    Set up mocha unit test and run first

---
+++ Inbox/test/Inbox.test.js
@@ -0,0 +1,25 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const { describe, it } = require("mocha");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+
+class Car {
+  park = () => "stopped";
+  drive = () => "vroom";
+}
+
+let car;
+beforeEach(() => {
+  car = new Car();
+});
+
+describe("Car", () => {
+  it("can park", () => {
+    assert.equal(car.park(), "stopped");
+  });
+
+  it("can drive", () => {
+    assert.equal(car.drive(), "vroom");
+  });
+});
