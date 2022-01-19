b97d0d629d22e619bb9a028c36808fe0936a138a
Andy Chen <andycjx8@gmail.com>
2022-01-19 11:33:28 +1100

    Get list of unlock accounts from ganache

--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -1,25 +1,16 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
-const { describe, it } = require("mocha");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
-class Car {
-  park = () => "stopped";
-  drive = () => "vroom";
-}
-
-let car;
 beforeEach(() => {
-  car = new Car();
+  //Get a list of all accounts
+  web3.eth.getAccounts().then((fetchedAccounts) => {
+    console.log(fetchedAccounts);
+  });
+  //Use one of those accounts to deploy the contract
 });
 
-describe("Car", () => {
-  it("can park", () => {
-    assert.equal(car.park(), "stopped");
-  });
-
-  it("can drive", () => {
-    assert.equal(car.drive(), "vroom");
-  });
+describe("Inbox", () => {
+  it("deploys a contract", () => {});
 });
