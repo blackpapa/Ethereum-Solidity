35d6a9e31e21b5ffd6798897de2a765258cb5910
Andy Chen <andycjx8@gmail.com>
2022-01-21 11:26:22 +1100

    Update with abi, evm

--- Inbox-updated/compile.js
+++ Inbox-updated/compile.js
@@ -7,4 +7,23 @@ const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 //Read the File
 const source = fs.readFileSync(inboxPath, "utf8");
 
-module.exports = solc.compile(source, 1).contracts[":Inbox"];
+//Add the expected JSON formatted input, specifying the language, sources, and outputSelection
+const input = {
+  language: "Solidity",
+  sources: {
+    "Inbox.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+  "Inbox.sol"
+].Inbox;
--- Inbox-updated/test/Inbox.test.js
+++ Inbox-updated/test/Inbox.test.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
-const { interface, bytecode } = require("../compile");
+const { abi, evm } = require("../compile");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
@@ -12,9 +12,9 @@ beforeEach(async () => {
   //Get a list of all accounts
   accounts = await web3.eth.getAccounts();
   //Use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  inbox = await new web3.eth.Contract(abi)
     .deploy({
-      data: bytecode,
+      data: evm.bytecode.object,
       arguments: [INITIAL_MESSAGE],
     })
     .send({
@@ -26,7 +26,6 @@ beforeEach(async () => {
 describe("Inbox", () => {
   it("deploys a contract", () => {
     assert.ok(inbox.options.address);
-    console.log(inbox.options);
   });
   it("has a default meassage", async () => {
     const message = await inbox.methods.message().call();
